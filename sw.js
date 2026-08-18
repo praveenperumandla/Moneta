be53c19bb5c705d8b452254151e1752962d3ab28
praveenperumandla <praveen.k.86@gmail.com>
2026-08-19 01:05:46 +0530

    fix(nav): switch tab bar from fixed to in-flow flex dock at physical bottom edge v5.4.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'moneta-v5.4.3';
+const CACHE_NAME = 'moneta-v5.4.4';
 const basePath = self.location.pathname.replace(/\/sw\.js$/, '').replace(/\/$/, '') || '';
 const STATIC_ASSETS = [
   basePath + '/',
